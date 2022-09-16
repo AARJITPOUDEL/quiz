9e09c0f5e6eae483be8dcc438f8a9d1cec32bd0f
Aarjit <98299656+AARJITPOUDEL@users.noreply.github.com>
2022-09-15 20:29:12 -0700

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,68 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does QBASIC stand for?",
+    answer: "Quick Beginners All Purpose Symbolic Instruction Code",
+    options: [
+      "Quick Beginners All Purpose Symbolic Instruction Code",
+      "Quickly based symbol code",
+      "Quick Quick Symbol Coding",
+     
+    ]
+  },
+    {
+    numb: 4,
+    question: "Who made this Web-App?",
+    answer: "Aarjit",
+    options: [
+      "Aarjit",
+      "Aarjit",
+      "Aarjit",
+      "Aarjit"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Who is hero?",
+    answer: "Aarjit",
+    options: [
+      "Aarjit",
+      "Aarjit",
+      "Aarjit",
+      "Aarjit",
+    ]
+  },
+  {
+    numb: 6,
+    question: "Who is hero?",
+    answer: "Aarjit",
+    options: [
+      "Aarjit",
+      "Aarjit",
+      "Aarjit",
+      "Aarjit",
+    ]
+  },
+];
